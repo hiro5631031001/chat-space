81b2a22b96a37759513ce4af6d3c496b43715dba
hiro5631031001 <hiro5631031001@gmail.com>
2020-03-07 19:02:38 +0900

    message_ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,41 @@
 $(function(){
+    function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image) {
+      var html =  `<div class="message">
+                    <div class="message__info">
+                    <p class="message__info__user">
+                      ${ message.user_name }
+                    </p>
+                    <p class="message__info__date">
+                      ${ message.created_at }
+                    </p>
+                    </div>
+                    <p class="message__text">
+                      ${ message.text}
+                    <img src = ${message.image}>
+                    </p>
+                  </div>`
+      return html;
+    } else {
+      var html = `<div class="message">
+                    <div class="message__info">
+                    <p class="message__info__user">
+                     ${ message.user_name }
+                    </p>
+                    <p class="message__info__date">
+                     ${ message.created_at }
+                    </p>
+                    </div>
+                    <p class="message__text">
+                     ${message.text}
+                    </p>
+                  </div>`
+      return html;
+    };
+  }
+
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +47,16 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-chat__message-list').append(html);
+      $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.submit-btn').prop('disabled', false);
+    })
+    .fail(function(){
+      alert("メッセージの送信に失敗しました");
     })
   });
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,9 +8,10 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージを作成しました'
+      respond_to do |format|
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください'
