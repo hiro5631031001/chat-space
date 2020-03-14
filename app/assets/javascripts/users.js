b730c76bfdc7d45e5f0e0868dab6ac202a8b959b
hiro5631031001 <hiro5631031001@gmail.com>
2020-03-14 18:43:15 +0900

    Update_users.js(add_id)

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -21,7 +21,7 @@ $(function(){
 
   function addDeleteUse(name, id) {
     var html = `
-    <div class="chat-group-user clearfix">
+    <div class="chat-group-user clearfix" id="${id}">
       <p class="chat-group-user__name">${name}</p>
       <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${id}" data-user-name="${name}">削除</div>
     </div>
