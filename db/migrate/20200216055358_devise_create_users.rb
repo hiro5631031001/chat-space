62f6e034c23865db55c7a6ad6442e62be1a53680
hiro5631031001 <hiro5631031001@gmail.com>
2020-03-15 21:07:16 +0900

    Update 20200216055358_devise_create_users.rb

--- db/migrate/20200216055358_devise_create_users.rb
+++ db/migrate/20200216055358_devise_create_users.rb
@@ -7,15 +7,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-    end
-    add_index :users, unique: true
+
 
     #   ## Recoverable
-    #   t.string   :reset_password_token
-    #   t.datetime :reset_password_sent_at
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
     #   ## Rememberable
-    #   t.datetime :remember_created_at
+      t.datetime :remember_created_at
 
     #   ## Trackable
     #   # t.integer  :sign_in_count, default: 0, null: false
@@ -36,11 +35,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
     #   # t.datetime :locked_at
 
 
-    #   t.timestamps null: false
-    # end
+      t.timestamps null: false
+    end
 
-    # add_index :users, :email,                unique: true
-    # add_index :users, :reset_password_token, unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
     # # add_index :users, :confirmation_token,   unique: true
     # # add_index :users, :unlock_token,         unique: true
   end
