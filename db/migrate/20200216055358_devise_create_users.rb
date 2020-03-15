a23198334c3d3add8ae8a7ecbed4eee8c34a67a9
hiro5631031001 <hiro5631031001@gmail.com>
2020-03-15 21:18:53 +0900

    Update 20200216055358_devise_create_users.rb

--- db/migrate/20200216055358_devise_create_users.rb
+++ db/migrate/20200216055358_devise_create_users.rb
@@ -10,11 +10,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
 
 
     #   ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
     #   ## Rememberable
-      t.datetime :remember_created_at
+      # t.datetime :remember_created_at
 
     #   ## Trackable
     #   # t.integer  :sign_in_count, default: 0, null: false
